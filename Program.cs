65cea2bc121f7cda85cd40f36dd3fd4ee8737299
skcircle <496103864@qq.com>
2024-07-02 08:30:38 +0800

    强制hosting控制台窗口不允许关闭。

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Skcircle.HalconHosting
 	{
 		static void Main(string[] args)
 		{
+			ConsoleBase.DisableCloseButton(Console.Title);
 			using (ServiceHost host = new ServiceHost(typeof(HalconOperator)))
 			{
 				host.Opened += delegate
@@ -22,6 +24,27 @@ namespace Skcircle.HalconHosting
 				host.Open();
 				Console.Read();
 			}
+		}
+	}
+
+	public class ConsoleBase
+	{
+		[DllImport("User32.dll", EntryPoint = "FindWindow")]
+		static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+		[DllImport("user32.dll", EntryPoint = "GetSystemMenu")]
+		static extern IntPtr GetSystemMenu(IntPtr hWnd, IntPtr bRevert);
+
+		[DllImport("user32.dll", EntryPoint = "RemoveMenu")]
+		static extern IntPtr RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
+
+		public static void DisableCloseButton(string title)
+		{
+			System.Threading.Thread.Sleep(100);
+			IntPtr windowHandle = FindWindow(null, title);
+			IntPtr closeMenu = GetSystemMenu(windowHandle, IntPtr.Zero);
+			uint SC_CLOSE = 0xF060;
+			RemoveMenu(closeMenu, SC_CLOSE, 0x0);
 		}
 	}
 }
