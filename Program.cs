06dc27671e5eeeac838e8d37f85bdbdeaa8c993c
Administrator <Administrator@CYE8GDOEVM74V9Y>
2024-07-02 08:04:00 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Skcircle.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skcircle.HalconHosting
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			using (ServiceHost host = new ServiceHost(typeof(HalconOperator)))
+			{
+				host.Opened += delegate
+				  {
+					  Console.WriteLine("HalconService已经启动，按任意键终止服务！");
+				  };
+
+				host.Open();
+				Console.Read();
+			}
+		}
+	}
+}
